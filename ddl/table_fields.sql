3e9448edeca3a951420b41289bc3715a459ba542
Simon Fermor <simon.fermor@gmail.com>
2023-11-07 07:56:39 -0600

    Change name for table_fields column

--- ddl/table_fields.sql
+++ ddl/table_fields.sql
@@ -15,15 +15,17 @@
 CREATE TABLE IF NOT EXISTS `table_fields` (
   `_id` int(11) NOT NULL AUTO_INCREMENT,
   `table_id` int(11) DEFAULT NULL,
-  `position` int(11) DEFAULT NULL,
   `field_id` int(11) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `order_key` int(11) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` timestamp NULL DEFAULT NULL,
-  `sort_key` int(11) DEFAULT NULL,
   PRIMARY KEY (`_id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+-- Dumping data for table dictionary.table_fields: ~0 rows (approximately)
+/*!40000 ALTER TABLE `table_fields` DISABLE KEYS */;
+/*!40000 ALTER TABLE `table_fields` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
